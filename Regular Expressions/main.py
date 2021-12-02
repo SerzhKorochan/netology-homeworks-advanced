c17496f79a4518fbe06a22722a99f5c7aed6596f
SerzhKorochan <serzhkorochan@gmail.com>
2021-12-02 14:30:43 +0200

    program can create prepared name

--- Regular Expressions/main.py
+++ Regular Expressions/main.py
@@ -1,13 +1,41 @@
+import re
 from pprint import pprint
 import csv
 
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-pprint(contacts_list)
+# pprint(contacts_list)
 
 # TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
+corrected_phone_book = [contacts_list.pop(0)]
+
+
+def get_prepared_name(name: str):
+    pattern = re.compile("[А-Я][а-я]+")
+
+    prepared_name = pattern.findall(name)
+
+    if len(prepared_name) < 3:
+        for i in range(len(prepared_name), 3):
+            prepared_name.append('')
+
+    return prepared_name
+
+
+for item in contacts_list:
+    name = ''
+    person = []
+
+    for idx, field in enumerate(item):
+        if idx < 3: name += field
+
+        if idx == 3:
+            person = get_prepared_name(name)
+            corrected_phone_book.append(person)
+
+
+print(corrected_phone_book)
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
