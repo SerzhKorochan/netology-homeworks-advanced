0fe7f4e307e5e2372adabaad67b5b1ba11f6a4ff
SerzhKorochan <serzhkorochan@gmail.com>
2021-12-05 12:49:02 +0200

    Normalization of full name and phone number added

--- Regular Expressions/main.py
+++ Regular Expressions/main.py
@@ -12,18 +12,43 @@ class AddressBook:
         if os.path.exists(address_book_path):
             self.initial_book_df = pd.read_csv(address_book_path)
 
-    def split_column_by_pattern(self, column_name: str, separation_names: list, pattern = r'[А-Я][а-я]+'):
+    def __split_column_by_pattern(self, column_name: str, separation_names: list, pattern = r'[А-Я][а-я]+'):
         pattern = re.compile(pattern)
-        self.initial_book_df.insert(2, 'tmp', np.nan)
-        # for column_name in separation_names:
-        #     self.initial_book_df['tpm'] += self.initial_book_df[column_name]
+        self.initial_book_df['tmp'] = ''
+
+        for column_name in separation_names:
+            self.initial_book_df['tmp'] += self.initial_book_df[column_name].astype(str)
+
+        for idx, row in self.initial_book_df.iterrows():
+            parts_of_column = pattern.findall(row['tmp'])
+
+            for i in range(len(parts_of_column), len(separation_names)):
+                parts_of_column.append(np.nan)
+
+            for k in range(0, len(separation_names)):
+                row[separation_names[k]] = parts_of_column[k]
+
+        self.initial_book_df = self.initial_book_df.drop(columns=['tmp'])
 
         return True
 
+    def __replace_values_by_pattern(self, column_name: str, pattern: str, repl: str):
+        self.initial_book_df[column_name].replace(pattern, repl, inplace=True, regex=True)
+
+    def normalize(self):
+        #normalize full name
+        self.__split_column_by_pattern('lastname', ['lastname', 'firstname', 'surname'])
+
+        #normalize phone
+        self.__replace_values_by_pattern('phone',
+                                         r'(\+7|8)(\s+|\s+\(|\(|)([0-9]{3}|[0-9]{3})(\)\s+|\)|-|)([0-9]{3})'
+                                         r'(-|)([0-9]{2})(-|)([0-9]{2})(((\s+\(|\s+)|)(доб\.\s+[0-9]{4}|)(\)|\s+|))',
+                                         r'\1(\3)\5-\7-\9\13')
+
 
 my_disordered_book = AddressBook('phonebook_raw.csv')
-my_disordered_book.split_column_by_pattern('lastname', ['lastname', 'firstname', 'surname'])
-pprint(my_disordered_book.initial_book_df)
+my_disordered_book.normalize()
+pprint(my_disordered_book.initial_book_df[['lastname', 'firstname', 'phone']])
 
 
 # TODO 2: сохраните получившиеся данные в другой файл
