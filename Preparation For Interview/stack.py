aafd51a521718cadd0d4bc87dad8f8874e35b616
SerzhKorochan <serzhkorochan@gmail.com>
2021-12-14 12:51:08 +0200

    i corrected class "Stack" and finished task 2

--- Preparation For Interview/stack.py
+++ Preparation For Interview/stack.py
@@ -1,6 +1,6 @@
 class Stack:
-    def __init__(self, stack_elements: list):
-        self.stack_elements = stack_elements
+    def __init__(self):
+        self.stack_elements = []
 
     def is_empty(self) -> bool:
         if len(self.stack_elements):
@@ -18,3 +18,27 @@ class Stack:
 
     def size(self) -> int:
         return len(self.stack_elements)
+
+
+def is_brackets_balanced(subsequence: str) -> bool:
+    pairs = {'(': ')', '[': ']', '{': '}'}
+    stack = Stack()
+
+    for bracket in subsequence:
+        if bracket in '([{':
+            stack.push(bracket)
+        elif stack.size() and bracket == pairs[stack.peek()]:
+            stack.pop()
+        else:
+            return False
+
+    return stack.size() == 0
+
+
+brackets = '{{}}'
+res = is_brackets_balanced(brackets)
+
+if res:
+    print('Balanced!')
+else:
+    print('Unbalanced!')
